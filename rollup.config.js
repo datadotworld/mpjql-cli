cb1fb344283a7972f64a76f495c64179e2f0bb0e
Shad <shad.reynolds@gmail.com>
2017-10-11 14:58:44 -0500

    allow for importing of the code

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,28 @@
 import pkg from './package.json'
+const external = [
+  'fs',
+  'yargs',
+  'axios',
+  'rollup',
+  'rollup-plugin-babel',
+  'rollup-plugin-replace',
+  'rollup-plugin-uglify',
+  'babel-preset-es2015-rollup',
+  'querystring'
+]
 
 export default [
   {
     entry: './src/main.js',
-    external: [
-      'fs',
-      'yargs',
-      'axios',
-      'rollup',
-      'rollup-plugin-babel',
-      'rollup-plugin-replace',
-      'rollup-plugin-uglify',
-      'babel-preset-es2015-rollup',
-      'querystring'
-    ],
-
+    external: external,
+    targets: [
+      { dest: 'dist/cli.cjs.js', format: 'cjs' },
+      { dest: 'dist/cli.esm.js', format: 'es' }
+    ]
+  },
+  {
+    entry: './src/mixpanel.js',
+    external: external,
     targets: [
       { dest: pkg.main, format: 'cjs' },
       { dest: pkg.module, format: 'es' }
